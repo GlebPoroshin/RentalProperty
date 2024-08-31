cf2d537d38d9654f0e983817de41a1541f1d1e3a
Gleb Poroshin <glebporoshin@montekrist.media>
2024-08-31 23:20:25 +0300

    End migration. Add Optioonal<T>

--- src/main/java/com/gleb/rentservice/repositories/BaseRepository.java
+++ src/main/java/com/gleb/rentservice/repositories/BaseRepository.java
@@ -4,13 +4,14 @@ import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface BaseRepository<EntityType, ID> extends Repository<EntityType, ID> {
 
    void save(EntityType entity);
 
-    EntityType findById(ID id);
+    Optional<EntityType> findById(ID id);
 
     List<EntityType> findAll();
 
